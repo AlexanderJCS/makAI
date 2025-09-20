65dc79d43ddf99cfc814db8346490e79512432ef
Alex <alexandercastronovo@gmail.com>
2025-09-19 23:53:33 -0400

    change to new node

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
-import responses from "./responses.json" assert { type: "json" };
+import responses from "./responses.json" with { type: "json" };
 import {
   ButtonStyleTypes,
   InteractionResponseFlags,
